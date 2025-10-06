789830a6d0c22edd59c986bdc5fec3d0e272d30a
atosh <atoshmohonto11@gmail.com>
2025-10-06 11:54:58 +0600

    do while loop

---
+++ basic_book_c/07.control statement/loop/do while loop/hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i;
+    i=0;
+    do{
+        printf("hello world!\n");
+    }
+    while(i!=0);
+    return 0;
+}
---
+++ basic_book_c/07.control statement/loop/for loop/1+2^2+...+n^2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+    printf("Enter the values:");
+    scanf("%d",&n);
+   
+    return 0;
+}

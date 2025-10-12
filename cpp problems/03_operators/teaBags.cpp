a52a35371cb8d7d6b0d4aef06989d3bc923d09b2
Atosh Mohonto <135542605+AtoshMohonto@users.noreply.github.com>
2025-10-12 10:49:49 +0600

    tea bags

---
+++ cpp problems/03_operators/teaBags.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int teaBags;
+    cout<<"Enter the numbers of tea bags:" ;
+    cin>> teaBags;
+    if(teaBags < 10){
+       // teaBags= teaBags + 5;
+       teaBags+=5;
+        }
+  cout<< "Your total tea bags are:"<< teaBags <<endl;
+    return 0;
+}
